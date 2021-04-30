9049f34667076bc1ca9bb1b069d7dd5535a1352f
hzy <1257664828@qq.com>
2021-04-30 14:38:02 +0800

    数据类型

---
+++ test/base/data_type.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main() {
+  // int a = pow(2, 64);
+  // print(a);
+
+  // var str = '12314';
+  // int a = num.parse(str);
+  // print(a);
+
+  var str = '123142a';
+  int a = num.tryParse(str);
+  print(a);
+}
