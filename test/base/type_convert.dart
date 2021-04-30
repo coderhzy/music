27c85a9818e05cf25ebcd5930af05c617f5a0d0a
hzy <1257664828@qq.com>
2021-04-30 14:11:41 +0800

    类型转换

---
+++ test/base/type_convert.dart
@@ -0,0 +1,22 @@
+// 类型转换
+void main() {
+  // 类提供的方法
+  // var a = 12;
+  // print(a.toString() + "xx");
+
+  // var a = '12';
+  // int b;
+  // b = int.parse(a);
+
+  // var a = 12;
+  // double b;
+  // b = a.toDouble();
+
+  // var arr = [1, 2, 3];
+  // Object o = arr;
+  // print((o as List)[0]);
+
+  var a = 12.5;
+  double b = a;
+  print(b);
+}
