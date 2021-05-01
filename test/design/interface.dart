82af77279a9ab29314500d9fe7b54645afe97a29
hzy <1257664828@qq.com>
2021-05-01 15:29:47 +0800

    接口

---
+++ test/design/interface.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+
+class Interface {
+  void show() {}
+  void hide() {}
+}
+
+class A implements Interface {
+  @override
+  void hide() {
+    // TODO: implement hide
+  }
+
+  @override
+  void show() {
+    // TODO: implement show
+  }
+}
+
+void main() {
+  // 子
+  Interface a;
+  a = A(); // 因为是静态类型
+}
