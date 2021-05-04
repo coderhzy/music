f105929d105c04844ec019106b0932eb29843867
hzy <1257664828@qq.com>
2021-05-04 17:38:44 +0800

    header开发

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:music/util/screen_util.dart';
+import 'package:music/pages/home/header.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var screen = Screen(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('首页'),),
-      body: Text('b'),
+      body: Container(
+        padding: EdgeInsets.only(top:screen.top + screen.calc(7)),
+        child: Column(
+          children: [
+           Header()
+          ],
+        ),
+      ),
     );
   }
 }
