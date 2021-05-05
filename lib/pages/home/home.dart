7328df168c85ab1631d4821b89b5c6a4d0a9fb6e
hzy <1257664828@qq.com>
2021-05-05 11:24:28 +0800

    推荐列表

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,6 +5,91 @@ import 'package:music/pages/home/banner.dart';
 import 'package:music/pages/home/category.dart';
 import 'package:music/widgets/song_list.dart';
 import 'package:music/widgets/recommend_list.dart';
+import 'package:music/widgets/global_bottom_navigation_bar.dart';
+
+const recommendList1 = [
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_1.jpg',
+    'title': 'Tempest',
+    'artist': 'Capo Productions',
+    'tip': '你到不了的地方 音乐能带你去'
+  },
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_2.jpg',
+    'title': '岁月神偷',
+    'artist': '金玟岐',
+    'tip': 'aaa'
+  },
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_3.jpg',
+    'title': '最美的期待',
+    'artist': '周笔畅',
+    'tip': 'bbb'
+  },
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_4.jpg',
+    'title': '遥远的你',
+    'artist': 'xxxx',
+    'tip': 'xxx'
+  },
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_5.jpg',
+    'title': '遥远的你',
+    'artist': 'xxxx',
+    'tip': 'xxx'
+  },
+  {
+    'id': '1111',
+    'img': 'assets/tmp_cover_6.jpg',
+    'title': '遥远的你',
+    'artist': 'xxxx',
+    'tip': 'xxx'
+  },
+];
+
+const songList1 = [
+  {
+    'id': '43423',
+    'img': 'assets/tmp_cover_1.jpg',
+    'count': '9亿',
+    'title': '[华语新歌] 最新华语音乐专辑|最快最多最好最什么'
+  },
+  {
+    'id': '666',
+    'img': 'assets/tmp_cover_2.jpg',
+    'count': '39.4亿',
+    'title': '今天从《Titan》听起|私人雷达'
+  },
+  {
+    'id': '231',
+    'img': 'assets/tmp_cover_3.jpg',
+    'count': '16亿',
+    'title': 'aaa'
+  },
+  {
+    'id': '333',
+    'img': 'assets/tmp_cover_4.jpg',
+    'count': '5321',
+    'title': 'bbb'
+  },
+  {
+    'id': '788',
+    'img': 'assets/tmp_cover_5.jpg',
+    'count': '19万',
+    'title': 'ccc'
+  },
+  {
+    'id': '344',
+    'img': 'assets/tmp_cover_6.jpg',
+    'count': '565',
+    'title': 'dddd'
+  },
+];
 
 class HomePage extends StatelessWidget {
   @override
@@ -24,54 +109,31 @@ class HomePage extends StatelessWidget {
                 MyBanner(),
                 CateGory(),
                 SongList(
-                    title: '你的歌单精选站',
-                    onMore: () {
-                      Navigator.pushNamed(context, '/square');
-                    },
-                    items: [
-                      {
-                        'id': '1',
-                        'img': 'assets/tmp_cover_1.jpg',
-                        'count': '9亿',
-                        'title': '[华语新歌]最新华宇音乐专辑|最酷，最快，最多，最好，最真'
-                      },
-                      {
-                        'id': '2',
-                        'img': 'assets/tmp_cover_2.jpg',
-                        'count': '39亿',
-                        'title': '[从你的]全世界路过'
-                      },
-                      {
-                        'id': '3',
-                        'img': 'assets/tmp_cover_3.jpg',
-                        'count': '99.2亿',
-                        'title': '[那些年]错过的大雨'
-                      },
-                      {
-                        'id': '4',
-                        'img': 'assets/tmp_cover_4.jpg',
-                        'count': '14223',
-                        'title': '[华语新歌]最新华晨宇新歌'
-                      },
-                      {
-                        'id': '5',
-                        'img': 'assets/tmp_cover_5.jpg',
-                        'count': '323亿',
-                        'title': '[华语新歌]李宇春新歌'
-                      },
-                      {
-                        'id': '6',
-                        'img': 'assets/tmp_cover_6.jpg',
-                        'count': '19万',
-                        'title': '[华语新歌]精选周杰伦'
-                      },
-                    ]),
-                RecommendListPage(),
+                  title: '你的歌单精选站',
+                  onMore: () {
+                    Navigator.pushNamed(context, '/square');
+                  },
+                  items: songList1,
+                ),
+                RecommendListPage(
+                  title: '根据 Red Down （a）推荐',
+                  items: recommendList1,
+                  onMore: () {
+                    print('more');
+                  },
+                  onPlay: (id) {
+                    print('play:$id');
+                  },
+                ),
+                Container(
+                  height: screen.calc(100),
+                )
               ],
             ),
           ),
         ),
       ),
+      bottomNavigationBar: GlobalBottomNavigationBar(),
     );
   }
 }
--- lib/widgets/recommend_list.dart
+++ lib/widgets/recommend_list.dart
@@ -1,10 +1,213 @@
 import 'package:flutter/material.dart';
+import 'package:music/util/screen_util.dart';
+
+typedef PlayCallback = void Function(String);
 
 class RecommendListPage extends StatelessWidget {
-  const RecommendListPage({Key key}) : super(key: key);
+  final String title;
+  final VoidCallback onMore;
+  final List<Map> items;
+  final PlayCallback onPlay;
+
+  const RecommendListPage(
+      {Key key, this.title, this.onMore, this.items, this.onPlay})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screen = Screen(context);
+
+    return Container(
+      margin: EdgeInsets.only(top: screen.calc(70)),
+      child: Column(
+        children: [
+          _Header(
+            title: title,
+            onMore: onMore,
+          ),
+          _Body(
+            items: items,
+            onPlay: onPlay,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// 推荐头部
+class _Header extends StatelessWidget {
+  final String title;
+  final VoidCallback onMore;
+  const _Header({
+    Key key,
+    this.title,
+    this.onMore,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen = Screen(context);
+
+    return Container(
+      padding: EdgeInsets.only(left: screen.calc(32), right: screen.calc(32)),
+      height: screen.calc(52),
+      child: Row(
+        children: [
+          Center(
+              child: Text(title,
+                  style: TextStyle(
+                    fontSize: screen.calc(34),
+                    fontWeight: FontWeight.w700,
+                  ))),
+          Spacer(),
+          GestureDetector(
+              onTap: onMore,
+              child: Container(
+                padding: EdgeInsets.only(
+                    left: screen.calc(24), right: screen.calc(24)),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Color(0xffe7e7e7), width: screen.calc(2)),
+                  borderRadius: BorderRadius.circular(52 / 2),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.play_arrow),
+                    Text('播放全部'),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<Map> items;
+  final PlayCallback onPlay;
+
+  _Body({Key key, this.items, this.onPlay}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen = Screen(context);
+
+    final columns = [];
+    for (var i = 0; i < items.length; i += 3) {
+      columns.add(items.sublist(i, i + 3));
+    }
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        padding: EdgeInsets.only(top: screen.calc(4), left: screen.calc(32)),
+        child: Row(
+            children: columns
+                .map<Widget>((arr) => Column(
+                      children: arr
+                          .map<Widget>(
+                              (data) => _Item(data: data, onPlay: onPlay))
+                          .toList(),
+                    ))
+                .toList()),
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  final Map data;
+  final PlayCallback onPlay;
+
+  _Item({Key key, this.data, this.onPlay}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen = Screen(context);
+
+    return Container(
+      width: screen.calc(641),
+      height: screen.calc(120),
+      padding: EdgeInsets.only(top: screen.calc(20)),
+      margin: EdgeInsets.only(right: screen.calc(45)),
+      child: Row(
+        children: [
+          Container(
+            width: screen.calc(120),
+            height: screen.calc(120),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(data['img']),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding:
+                  EdgeInsets.only(left: screen.calc(20), top: screen.calc(16)),
+              alignment: Alignment.centerLeft,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text.rich(
+                      TextSpan(children: [
+                        TextSpan(text: '${data['title']} '),
+                        TextSpan(
+                            text: ' - ${data['artist']}',
+                            style: TextStyle(
+                              color: Color(0xff9a9a9a),
+                            )),
+                      ]),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: screen.calc(4), right: screen.calc(4)),
+                          decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Color(0xffff3a3a), width: 1),
+                            borderRadius: BorderRadius.circular(screen.calc(4)),
+                          ),
+                          child: Text('SQ',
+                              style: TextStyle(
+                                  color: Color(0xffff3a3a),
+                                  fontSize: screen.calc(20))),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: screen.calc(8)),
+                          child: Text(data['tip'],
+                              style: TextStyle(color: Color(0xff9a9a9a))),
+                        ),
+                      ],
+                    ),
+                  ]),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              onPlay(data['id']);
+            },
+            child: Container(
+              child: Center(
+                child: Container(
+                  height: screen.calc(50),
+                  width: screen.calc(50),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Color(0xffe6e6e6), width: screen.calc(2)),
+                      borderRadius: BorderRadius.circular(screen.calc(25))),
+                  child: Icon(Icons.play_arrow,
+                      size: screen.calc(30), color: Color(0xffff3a3a)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
