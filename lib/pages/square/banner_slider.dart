ff8b72f6dd867181fbfa929f17d9beaefa4e6bc3
hzy <1257664828@qq.com>
2021-05-06 18:55:39 +0800

    3D焦点图手势滑动

--- lib/pages/square/banner_slider.dart
+++ lib/pages/square/banner_slider.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 /// prev = (n - 1 + 10) % 10   , cur = n , next = (n + 1) % 10;
 
+/// 定义数据
 var items = [
   {'index': 0, 'title': '111'},
   {'index': 1, 'title': '222'},
@@ -15,117 +16,190 @@ var items = [
 ];
 
 class BannerSlider extends HookWidget {
-  const BannerSlider({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final screen = Screen(context);
     final _value = useState(0);
 
-    /// prev &  cur & next 对应的信息，具体看本文件最上方
+    /// 定位位置参数
     final prev = (_value.value - 1 + items.length) % items.length,
         cur = _value.value,
         next = (_value.value + 1) % items.length;
 
-    final W = screen.calc(344.0),
-        H = screen.calc(438.0),
-        offsetH = screen.calc(200),
-        offsetV = 20.0;
+    /// 图片以及常用大小
+    final W = screen.calc(344),
+        H = screen.calc(438),
+        offsetH = screen.calc(200);
 
-    /// prevRect & curRect & nextRect & otherRect 位置信息
-    Rect prevRect = Rect.fromLTWH(
-            (screen.width - W * 0.8) / 2 - offsetH, offsetV, W * .8, H * .8),
-        curRect = Rect.fromLTWH((screen.width - W) / 2, 0, W, H),
-        nextRect = Rect.fromLTWH(
-            (screen.width - W * 0.8) / 2 + offsetH, offsetV, W * .8, H * .8),
-        otherRect = Rect.fromLTWH((screen.width - W) / 2, 0, W, H);
+    /// prev & next透明度一样，cur透明度1.0
+    final secondaryData = {'scale': .8, 'opacity': .5},
+        curData = {'scale': 1.0, 'opacity': 1.0},
+        otherData = {'scale': .8, 'opacity': 0.0};
 
-    /// prev & cur & next & other 透明度
-    double prevOpacity = 0.5,
-        curOpacity = 1.0,
-        nextOpacity = 0.5,
-        otherOpacity = 0.0;
-
-    /// 根据透明度来排序item数组
     items.sort((item1, item2) {
       double opacity1, opacity2;
 
-      if (item1['index'] == prev)
-        opacity1 = prevOpacity;
+      if (item1['index'] == prev || item1['index'] == next)
+        opacity1 = secondaryData['opacity'];
       else if (item1['index'] == cur)
-        opacity1 = curOpacity;
-      else if (item1['index'] == next)
-        opacity1 = nextOpacity;
+        opacity1 = curData['opacity'];
       else
-        opacity1 = otherOpacity;
+        opacity1 = otherData['opacity'];
 
-      if (item2['index'] == prev)
-        opacity2 = prevOpacity;
+      if (item2['index'] == prev || item2['index'] == next)
+        opacity2 = secondaryData['opacity'];
       else if (item2['index'] == cur)
-        opacity2 = curOpacity;
-      else if (item2['index'] == next)
-        opacity2 = nextOpacity;
+        opacity2 = curData['opacity'];
       else
-        opacity2 = otherOpacity;
+        opacity2 = otherData['opacity'];
 
-      /// opacity从小到大排序
+      /// 根据opacity进行从小到大排序
       if (opacity1 < opacity2)
-        return 1;
-      else if (opacity1 > opacity2) return -1;
+        return -1;
+      else if (opacity1 > opacity2) return 1;
       return 0;
     });
 
-    print(items);
+    return GestureDetector(
+        onHorizontalDragEnd: (detail) {
+          /// 根据用户手势左右滑动的velocity(速度)来控制
+          var dx = detail.velocity.pixelsPerSecond.dx;
 
-    return Container(
-      margin: EdgeInsets.only(top: screen.calc(38)),
-      width: screen.width,
-      height: 200,
-      child: Stack(
-        /// map数据item生成Positioned组件
-        children: items.map((item) {
-          Rect rect;
-          double opacity;
-          int index = item['index'];
-
-          /// 在排序后的数组中进行判断index等于哪个位置，再将其设置opacity(透明度)和curRect(位置信息)
-          if (index == cur) {
-            rect = curRect;
-            opacity = curOpacity;
-          } else if (index == prev) {
-            rect = prevRect;
-            opacity = prevOpacity;
-          } else if (index == next) {
-            rect = nextRect;
-            opacity = nextOpacity;
-          } else {
-            rect = otherRect;
-            opacity = otherOpacity;
+          /// 给dx加判断，防止用户误触
+          if (dx.abs() >= 50) {
+            if (dx < 0) {
+              _value.value = (_value.value + 1) % items.length;
+            } else {
+              _value.value = (_value.value - 1 + items.length) % items.length;
+            }
           }
+        },
+        child: Container(
+          margin: EdgeInsets.only(top: screen.calc(38)),
+          height: H,
+          child: Stack(
+            children: items.map((item) {
+              var index = item['index'];
+              var data;
 
-          return Positioned(
-            left: rect.left,
-            top: rect.top,
-            child: Opacity(
-              opacity: opacity,
-              child: Container(
-                width: rect.width,
-                height: rect.height,
-                decoration: BoxDecoration(color: Colors.yellow),
-                child: Center(
-                    child: Text(
-                  item['title'],
-                  style: TextStyle(fontSize: 40),
-                )),
-              ),
-            ),
-          );
-        }).toList(),
-      ),
-    );
+              /// 判断index和cur，将curData对象赋值给{}，其他同理
+              if (index == cur)
+                data = curData;
+              else if (index == prev || index == next)
+                data = secondaryData;
+              else
+                data = otherData;
+
+              var offset = Offset(0, 0);
+
+              /// 判断左移右移，设置Offset位移距离
+              if (index == prev)
+                offset = Offset(-offsetH, 0);
+              else if (index == next) offset = Offset(offsetH, 0);
+
+              return Transform.translate(
+                  offset: offset,
+                  child: Transform.scale(
+                      scale: data['scale'],
+                      child: Opacity(
+                          opacity: data['opacity'],
+                          // opacity: 1,
+                          child: Container(
+                            width: W,
+                            height: H,
+                            decoration: BoxDecoration(color: Colors.blue),
+                            child: Center(
+                                child: Text(item['title'],
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ))),
+                          ))));
+            }).toList(),
+          ),
+        ));
   }
 }
 
-// 1. 对item数组的opacity进行从小到大排序
-// 2. 在排序后的数组中进行判断index等于哪个位置，再将其设置opacity(透明度)和curRect(位置信息)
-// 3. 根据item数组遍历生成Positioned，使用Rect,prevRect,curRect,nextRect,otherRect来控制位置
+// class BannerSlider extends HookWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     final screen=Screen(context);
+//     final _value=useState(0);
+//
+//     final
+//       prev=(_value.value-1+items.length)%items.length,
+//       cur=_value.value,
+//       next=(_value.value+1)%items.length;
+//
+//     final W=screen.calc(344),H=screen.calc(438),offsetH=screen.calc(200);
+//     final L=(screen.width-W)/2;
+//
+//     final
+//       secondaryData={'scale': .8, 'opacity': .5},
+//       curData={'scale': 1.0, 'opacity': 1.0},
+//       otherData={'scale': .8, 'opacity': 0.0};
+//
+//     items.sort((item1, item2){
+//       double opacity1,opacity2;
+//
+//       if(item1['index']==prev || item1['index']==next) opacity1=secondaryData['opacity'];
+//       else if(item1['index']==cur) opacity1=curData['opacity'];
+//       else opacity1=otherData['opacity'];
+//
+//       if(item2['index']==prev || item2['index']==next) opacity2=secondaryData['opacity'];
+//       else if(item2['index']==cur) opacity2=curData['opacity'];
+//       else opacity2=otherData['opacity'];
+//
+//       if(opacity1<opacity2) return -1;
+//       else if(opacity1>opacity2) return 1;
+//       return 0;
+//     });
+//
+//     return Container(
+//       margin: EdgeInsets.only(top: screen.calc(38)),
+//       width: screen.width,
+//       height: H,
+//       child: Stack(
+//         children: items.map((item){
+//           var index=item['index'];
+//           var data;
+//
+//           if(index==cur) data=curData;
+//           else if(index==prev || index==next) data=secondaryData;
+//           else data=otherData;
+//
+//           var offset=Offset(0,0);
+//           if(index==prev)offset=Offset(-offsetH, 0);
+//           else if(index==next)offset=Offset(offsetH, 0);
+//
+//           return Positioned(
+//             left: L,
+//
+//             child: Transform.translate(
+//               offset: offset,
+//               child: Transform.scale(
+//                 scale: data['scale'],
+//                 child: Opacity(
+//                   opacity: data['opacity'],
+//                   // opacity: 1,
+//                   child: Container(
+//                     width: W,
+//                     height: H,
+//                     decoration: BoxDecoration(
+//                       color: Colors.red
+//                     ),
+//                     child: Center(
+//                       child: Text(item['title'], style: TextStyle(
+//                         fontSize: 40,
+//                       ))
+//                     ),
+//                   )
+//                 )
+//               )
+//             ),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
