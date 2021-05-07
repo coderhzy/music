8d4e381065e1c71dbf99650d5bbd524c2c7bb41c
hzy <1257664828@qq.com>
2021-05-07 20:02:24 +0800

    音乐播放器播放按钮完成

--- lib/pages/player/control_panel_first.dart
+++ lib/pages/player/control_panel_first.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:music/util/screen_util.dart';
 
+const iconItem = [
+  {'id': 1, 'img': 'assets/icon_heart_w.png'},
+  {'id': 2, 'img': 'assets/icon_download_w.png'},
+  {'id': 3, 'img': 'assets/icon_bell_w.png'},
+  {'id': 4, 'img': 'assets/icon_message_w.png'},
+  {'id': 5, 'img': 'assets/icon_3dots_w.png'},
+];
+
 class ControlPageOne extends StatelessWidget {
   const ControlPageOne({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final screen = Screen(context);
-    return Text('ControlPageOne');
+    return Container(
+      height: screen.calc(55),
+      margin: EdgeInsets.only(bottom: screen.calc(14)),
+      padding: EdgeInsets.only(left: screen.calc(24), right: screen.calc(24)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: iconItem
+            .map((item) => Container(
+                  child: Image.asset(item['img']),
+                  width: screen.calc(55),
+                  height: screen.calc(55),
+                ))
+            .toList(),
+      ),
+    );
   }
 }
--- lib/pages/player/control_panel_second.dart
+++ lib/pages/player/control_panel_second.dart
@@ -1,12 +1,59 @@
+// import 'package:flutter/material.dart';
+// import 'package:music/util/screen_util.dart';
+//
+// class ControlPageSecond extends StatelessWidget {
+//   const ControlPageSecond({Key key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final screen = Screen(context);
+//     return Text('ControlPageSecond');
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:music/util/screen_util.dart';
 
 class ControlPageSecond extends StatelessWidget {
-  const ControlPageSecond({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final screen = Screen(context);
-    return Text('ControlPageSecond');
+
+    return Container(
+      margin: EdgeInsets.only(bottom: screen.calc(32), top: screen.calc(14)),
+      height: screen.calc(120),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Center(
+            child: Image.asset('assets/icon-playloop-w.png',
+                width: screen.calc(55), height: screen.calc(55)),
+          ),
+          Center(
+            child: Image.asset('assets/icon-backward-w.png',
+                width: screen.calc(55), height: screen.calc(55)),
+          ),
+          Container(
+            height: screen.calc(120),
+            width: screen.calc(120),
+            decoration: BoxDecoration(
+                border: Border.all(width: screen.calc(2), color: Colors.white),
+                borderRadius: BorderRadius.circular(screen.calc(60))),
+            child: Center(
+              child: Image.asset('assets/icon-pause-w.png',
+                  width: screen.calc(55), height: screen.calc(55)),
+            ),
+          ),
+          Center(
+            child: Image.asset('assets/icon-forward-w.png',
+                width: screen.calc(55), height: screen.calc(55)),
+          ),
+          Center(
+            child: Image.asset('assets/icon-playlist-w.png',
+                width: screen.calc(55), height: screen.calc(55)),
+          ),
+        ],
+      ),
+    );
   }
 }
