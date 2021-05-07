b93c142144acc3dddcf05809e67c298a1ce01d9e
hzy <1257664828@qq.com>
2021-05-07 10:44:13 +0800

    歌单列表完成 & 广场页面开发完成

--- lib/pages/square/square.dart
+++ lib/pages/square/square.dart
@@ -12,7 +12,7 @@ import 'package:music/pages/square/banner_slider.dart';
 import 'package:music/pages/square/square_list.dart';
 
 /// 广场页栏目数据
-final items = [
+final categoryItems = [
   {'id': 0, 'title': '推荐', 'color': null},
   {'id': 1, 'title': '官方', 'color': null},
   {'id': 2, 'title': '精品', 'color': 0xffe7aa5a},
@@ -26,6 +26,22 @@ final items = [
   {'id': 10, 'title': '其他', 'color': null},
 ];
 
+/// 广场页面歌单推荐
+const listItems = [
+  {'id': 11, 'img': 'assets/tmp_cover_1.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 12, 'img': 'assets/tmp_cover_2.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 13, 'img': 'assets/tmp_cover_3.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 14, 'img': 'assets/tmp_cover_4.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 15, 'img': 'assets/tmp_cover_5.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 16, 'img': 'assets/tmp_cover_6.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 17, 'img': 'assets/tmp_cover_1.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 18, 'img': 'assets/tmp_cover_2.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 19, 'img': 'assets/tmp_cover_3.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 20, 'img': 'assets/tmp_cover_4.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 21, 'img': 'assets/tmp_cover_5.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+  {'id': 22, 'img': 'assets/tmp_cover_6.jpg', 'title': '钢琴摇滚 | 浪子赠予诗人的一纸情书'},
+];
+
 class SquarePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
@@ -34,48 +50,52 @@ class SquarePage extends HookWidget {
     final args = ModalRoute.of(context).settings.arguments as Map;
 
     /// 变更状态
-    final categoryID = useState(items[0]['id']);
+    final categoryID = useState(categoryItems[0]['id']);
 
     /// 判断Android去除最上方栏
     setStatusBarStyle(Brightness.light);
 
     print(args);
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(color: Colors.white),
-          ),
-          Container(
-            height: screen.calc(860),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xff959a99), Colors.white],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                // stops: [0, 0.2],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.white),
+            ),
+            Container(
+              height: screen.calc(860),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xff959a99), Colors.white],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  // stops: [0, 0.2],
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: screen.top + screen.calc(7)),
-            child: Column(
-              children: [
-                Header(),
-                Category(
-                  /// 广场页父级元素传递
-                  items: items,
-                  value: categoryID.value,
-                  onChanged: (id) {
-                    categoryID.value = id;
-                  },
-                ),
-                BannerSlider(),
-                SquareList(),
-              ],
+            Container(
+              padding: EdgeInsets.only(top: screen.top + screen.calc(7)),
+              child: Column(
+                children: [
+                  Header(),
+                  Category(
+                    /// 广场页父级元素传递
+                    items: categoryItems,
+                    value: categoryID.value,
+                    onChanged: (id) {
+                      categoryID.value = id;
+                    },
+                  ),
+                  BannerSlider(),
+                  SquareList(
+                    items: listItems,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: GlobalBottomNavigationBar(
         /// 完成换页操作
--- lib/pages/square/square_list.dart
+++ lib/pages/square/square_list.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:music/util/screen_util.dart';
 
 class SquareList extends StatelessWidget {
-  const SquareList({Key key}) : super(key: key);
+  /// 父级传参过来，数据在square中
+  final List<Map> items;
+
+  const SquareList({Key key, this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('SquareList');
+    final screen = Screen(context);
+    final rows = [];
+
+    /// 遍历item  +3 显示ROW方向图片
+    for (var i = 0; i < items.length; i += 3) {
+      rows.add(items.sublist(i, i + 3));
+    }
+
+    return Container(
+      margin: EdgeInsets.only(top: screen.calc(2)),
+      padding: EdgeInsets.only(left: screen.calc(32), right: screen.calc(32)),
+      child: Column(
+
+          /// 遍历rows
+          children: rows
+              .map<Widget>((row) => Container(
+                    margin: EdgeInsets.only(top: screen.calc(36)),
+                    child: Row(
+                      /// flex布局 均分中间
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: row
+                          .map<Widget>(
+                            (item) => Container(
+                                width: screen.calc(214),
+                                height: screen.calc(300),
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      item['img'],
+                                      width: screen.calc(214),
+                                      height: screen.calc(214),
+
+                                      /// 图片铺满
+                                      fit: BoxFit.fill,
+                                    ),
+                                    Container(
+                                      child: Text(
+                                        item['title'],
+                                        maxLines: 2,
+                                        style: TextStyle(
+                                            fontSize: screen.calc(24)),
+                                      ),
+                                    )
+                                  ],
+                                )),
+                          )
+                          .toList(),
+                    ),
+                  ))
+              .toList()),
+    );
   }
 }
